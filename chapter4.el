ea6537ecfdc768b92b34dbec3c97b9e0ac6adf3a
irvin <irvin.hwang@gmail.com>
2014-09-22 08:17:13 -0700

    on 4.3.1

---
+++ chapter4.el
@@ -0,0 +1,22 @@
+;;;4
+
+;;;4.1
+
+Tags
+
+;;;4.2
+(defun ez-beginninng-of-buffer ()
+  "Move point to beginning"
+  (interactive)
+  (push-mark)
+  (goto-char (point-min)))
+
+;;;4.3
+(defun ez-mark-whole-buffer ()
+  "mark the whole buffer"
+  (interactive)
+  (push-mark)
+  (push-mark (point-max) nil t)
+  (goto-char (point-min)))
+
+;;;4.3.1
